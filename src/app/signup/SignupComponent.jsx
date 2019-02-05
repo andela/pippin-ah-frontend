b3b15ac309ae6da680585a6c3a2558b9399e5ac3
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-05 21:30:40 +0100

    continues setup

---
+++ src/app/signup/SignupComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignupComponent = () => {
+    return (
+        <div className="signup-box">
+            <div className="signup-text">Sign Up</div>
+        </div>
+    )
+}
+
+export default SignupComponent
