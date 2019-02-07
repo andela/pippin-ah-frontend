9b881d3360adf639bcab33d4ecfca25c90b3694b
Ebenezer <ebenezerdonu@gmail.com>
2019-02-07 16:19:55 +0100

    [feature: #162727510] add login component.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Home } from './home'
-import { Signup } from './signup'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './home';
+import { Login } from './login';
 
 const App = () => (
   <Router>
     <Switch>
       <Route path="/" component={Home} exact />
-      <Route path="/signup" component={Signup} exact />
+      <Route path="/login" component={Login} exact />
     </Switch>
   </Router>
-)
+);
 
-export default App
+export default App;
---
+++ src/app/login/LoginComponent.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import RingLoaderComponent from '../loaders';
+import { constants } from './duck';
+import '../../style/auth.scss';
+import {
+  cancel,
+  facebook,
+  twitter,
+  google,
+  lock,
+  username,
+} from '../../../assets/images';
+
+const LoginComponent = ({ loginUser, loginState, history }) => {
+  const onFormSubmit = e => {
+    e.preventDefault();
+    const usernameOrEmail = e.target.elements.usernameOrEmail.value.trim();
+    const password = e.target.elements.password.value.trim();
+    loginUser(usernameOrEmail, password);
+    console.log('#####', loginState);
+  };
+  if (loginState === constants.LOGIN_SUCCESS) {
+    history.push('/');
+  }
+  return (
+    <div className="signup-box">
+      <img className="btn-close" src={cancel} alt="close" />
+      <div className="signup-text">
+        Sign <span className="up">In</span>
+      </div>
+      <div className="signup-wrapper">
+        <div className="social-auth">
+          <div className="btn-facebook">
+            <img className="social-icon" src={facebook} alt="facebook" />
+            <div className="fb-text-wrapper">
+              <span className="social-text">Sign in with Facebook</span>
+            </div>
+          </div>
+          <div className="btn-twitter">
+            <img className="social-icon" src={twitter} alt="twitter" />
+            <div className="twitter-text-wrapper">
+              <span className="social-text">Sign in with Twitter</span>
+            </div>
+          </div>
+          <div className="btn-google">
+            <img
+              className="social-icon google-icon"
+              src={google}
+              alt="google"
+            />
+            <div className="google-text-wrapper">
+              <span className="social-text">Sign in with Google</span>
+            </div>
+          </div>
+        </div>
+        <form onSubmit={onFormSubmit}>
+          <div className="input-group">
+            <img src={username} alt="email" />
+            <input
+              type="text"
+              name="usernameOrEmail"
+              placeholder="Username or Email"
+              required
+            />
+          </div>
+          <div className="input-group">
+            <img src={lock} alt="lock" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">
+              <span className="submit-text">SIGN IN</span>
+            </button>
+            <div className="signin-option">
+              <span className="account">
+                Have an account?
+                <span className="signin">Sign In</span>
+              </span>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="ring-loader">
+        {loginState === constants.LOGGING_IN && <RingLoaderComponent />}
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(LoginComponent);
