161f10b497639e3acbfb4401c5611d7acf19d061
Ebenezer <ebenezerdonu@gmail.com>
2019-02-07 20:29:29 +0100

    [feature: #162727510] add test file for loginComponent

--- src/app/login/login.test.js
+++ src/app/login/login.test.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import LoginContainer from './LoginContainer';
+import LoginComponent from './LoginComponent';
 
 describe('Login Component', () => {
   it('should render without throwing an error', () => {
-    expect(
-      shallow(<LoginContainer />)
-        .find('form.login')
-        .exists(),
-    ).toBe(true);
+    expect(shallow(<LoginComponent />).exists()).toBe(true);
   });
 });
