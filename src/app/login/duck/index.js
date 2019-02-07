a51ae3929e8d42874442c1f52f336a4a53ab14c1
Ebenezer <ebenezerdonu@gmail.com>
2019-02-07 16:20:41 +0100

    [feature: #162727510] export operations

---
+++ src/app/login/duck/index.js
@@ -0,0 +1,4 @@
+export { default as operations } from './operations';
+export { default as loginReducer } from './reducers';
+export { default as constants } from './constants';
+export { default as doLogin } from './operations';
