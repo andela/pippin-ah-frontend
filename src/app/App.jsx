94f8732ad806dcb6d72617f9782abef7f3949333
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-07 09:47:36 +0100

    [#162727508]: remove history

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Home } from './home'
 import { Signup } from './signup'
+import Modal from './signup/Modal'
 
 const App = () => (
-    <Router>
-        <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/signup" component={Signup} exact />
-        </Switch>
-    </Router>
+  <Router>
+    <Fragment>
+      <Modal />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/signup" component={Signup} exact />
+      </Switch>
+    </Fragment>
+  </Router>
 )
 
 export default App
