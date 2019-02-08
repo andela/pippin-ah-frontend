e68b5f5460285a258c420154c8c203c4f7c29749
Ebenezer <ebenezerdonu@gmail.com>
2019-02-08 18:46:57 +0100

    [feature: #162727510] add test for login component

--- src/app/login/login.test.js
+++ src/app/login/login.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import LoginComponent from './LoginComponent';
+import { LoginComponent as Login } from './LoginComponent';
 
 describe('Login Component', () => {
   it('should render without throwing an error', () => {
-    expect(shallow(<LoginComponent />).exists()).toBe(true);
+    const loginUser = jest.fn();
+    const wrapper = shallow(<Login loginUser={loginUser} />);
+    wrapper.find('form').simulate('submit', {
+      target: {
+        elements: {
+          usernameOrEmail: {
+            value: 'spicy',
+          },
+          password: {
+            value: 'dicy',
+          },
+        },
+      },
+      preventDefault: () => {},
+    });
+    expect(loginUser).toHaveBeenCalledWith('spicy', 'dicy');
+  });
+});
+
+describe('Login Component', () => {
+  it('should render an email input', () => {
+    expect(shallow(<Login />).find('#usernameOrEmail').length).toEqual(1);
+  });
+
+  it('should render a password input', () => {
+    expect(shallow(<Login />).find('#password').length).toEqual(1);
   });
 });
