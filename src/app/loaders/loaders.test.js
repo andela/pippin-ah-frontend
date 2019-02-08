eaf2fd4ca61b79b4981209d3d6675074db0028a8
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-08 19:51:03 +0100

    [#162727508]: add test for RingLoaderComponent

---
+++ src/app/loaders/loaders.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import RingLoaderComponent from '.';
+
+describe('It should render the RingLoader', () => {
+  it('should render the Landing Page', () => {
+    const component = shallow(<RingLoaderComponent />);
+    expect(component.exists()).toBe(true);
+    expect(component).toMatchSnapshot();
+  });
+});
