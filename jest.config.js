b6b71c80cc7fa8b1e89028228df7ac56ff188873
habibaudu <auduhabib1990@gmail.com>
2019-02-08 15:31:51 +0100

    feat(): effect feedback on navbar [#163722982]

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    setupFiles: ['./testSetup.js'],
-    transform: { '^.+\\.js|jsx$': 'babel-jest' },
+    setupFiles: ['<rootDir>/src/tests/setup.js'],
+    transform: { '^.+\\.jsx?$': 'babel-jest' },
     collectCoverageFrom: ['src/**/*.{js,jsx}'],
     snapshotSerializers: ['enzyme-to-json/serializer'],
     moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|svg)$': 'identity-obj-proxy',
-        '\\.(css|scss)$': 'identity-obj-proxy',
+        '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/filemock.js',
+        '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
     },
 }
