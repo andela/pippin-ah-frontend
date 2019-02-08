224b8d6de81220fa4db4b05dde7ab4f544f90661
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-08 19:52:12 +0100

    [#162727508]: remove comment

--- src/app/signup/duck/types.js
+++ src/app/signup/duck/types.js
@@ -1,5 +1,3 @@
 const SET_SIGNUP_STATE = 'SET_SIGNUP_STATE';
 const SET_SIGNUP_ERROR = 'SET_SIGNUP_ERROR';
-// const  signingUp = 'STATE_LOADING';
-// const  signUpError
 export default { SET_SIGNUP_STATE, SET_SIGNUP_ERROR };
