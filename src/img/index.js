5644539ba8077a068213b7ee303fc22598e7a920
xrole <xrolediamond@gmail.com>
2019-02-11 10:38:15 +0100

    [feature: 163845211] Move signUp images to img root directory

--- src/img/index.js
+++ src/img/index.js
@@ -1,1 +1,7 @@
-export * from './signup';
+export { default as cancel } from './cancel.svg';
+export { default as facebook } from './facebook.svg';
+export { default as twitter } from './twitter.svg';
+export { default as google } from './google.svg';
+export { default as email } from './email.svg';
+export { default as lock } from './lock.svg';
+export { default as username } from './username.svg';
--- src/img/signup/index.js
+++
@@ -1,8 +0,0 @@
-export { default as cancel } from './cancel.svg';
-export { default as email } from './email.svg';
-export { default as facebook } from './facebook.svg';
-export { default as google } from './google.svg';
-export { default as key } from './key.svg';
-export { default as lock } from './lock.svg';
-export { default as twitter } from './twitter.svg';
-export { default as username } from './username.svg';
