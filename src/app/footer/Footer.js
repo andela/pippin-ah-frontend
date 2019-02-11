6eef3101cc3419330ba0e2291f0d9083a9dd75c7
xrole <xrolediamond@gmail.com>
2019-02-11 09:19:19 +0100

    [feature: #163845211] Restructure files and folders move all images to img directory move all component styles to respective component module refactore the import and export routes remove old snapshots and recreate new ones [Finishes: 163845211]

--- src/app/footer/Footer.js
+++ src/app/footer/Footer.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import '../../style/footer.scss';
+import './footer.scss';
 
 library.add(fas, fab);
 
--- src/app/home/HomeComponent.jsx
+++ src/app/home/HomeComponent.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import '../../style/home.scss';
+import './home.scss';
 
 const HomeComponent = () => {
   return (
