a3e0f622242906d34cef9e0d895b7b4c632f7354
Ebenezer <ebenezerdonu@gmail.com>
2019-02-13 21:34:43 +0100

    [Feature: #162727509] import loader components

--- src/app/signup/SignupComponent.jsx
+++ src/app/signup/SignupComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import RingLoaderComponent from '../loaders';
+import { RingLoaderComponent } from '../loaders';
 import { constants } from './duck';
 import './signup.scss';
 import {
