3c789be49ae93a00460104f43c7dfd0594f586e1
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-13 08:48:45 +0100

    [#163860915]: setup feature files

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './home';
 import { Signup } from './signup';
 import { Login } from './login';
-import Article from './article';
+import { Article, CreateArticle } from './article';
 import Footer from './footer';
 import Navbar from './navBar/NavbarComponent';
 
@@ -16,6 +16,7 @@ const App = () => (
         <Route path="/signup" component={Signup} exact />
         <Route path="/login" component={Login} exact />
         <Route path="/articles" component={Article} exact />
+        <Route path="/create-article" component={CreateArticle} exact />
       </Switch>
       <Footer />
     </Fragment>
---
+++ src/app/article/CreateArticleComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CreateArticleComponent = () => {
+  return <div> CreateArticleComponent</div>;
+};
+
+export default CreateArticleComponent;
