b3b6ddc4f49b4eca84f3b6971e671e5f5f29024d
Ebenezer <ebenezerdonu@gmail.com>
2019-02-13 17:39:14 +0100

    [Feature: #162727509] refactor resetPassword component

--- src/app/resetPassword/resetPasswordComponent.jsx
+++ src/app/resetPassword/resetPasswordComponent.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import RingLoaderComponent from '../loaders';
+import { constants } from './duck';
 import './resetPassword.scss';
 
-const resetPasswordComponent = () => {
+const resetPasswordComponent = ({ resetPassword, resetPasswordState }) => {
+  const onFormSubmit = e => {
+    e.preventDefault();
+    const email = e.target.elements.email.value.trim();
+    resetPassword(usernameOrEmail, password);
+  };
+  /* if (resetPasswordState === constants.RESET_SUCCESS) {
+    return <Redirect to="/" />;
+  } */
   return (
     <div className="reset-password-container">
-      <div className="reset-page-title">
-        <h2>Let's help you get back your account</h2>
-      </div>
+      <div className="reset-password">
+        <div className="reset-page-title">
+          <h2>Let's help you get back your account</h2>
+        </div>
 
-      <div className="reset-row">
-        <p className="reset-text">Enter your email address here.</p>
-        <form id="reset">
-          <input className="resetEmail" id="resetEmail" type="email" />
-        </form>
-        <button
-          id="submit"
-          className="btn reset-button"
-          type="submit"
-          name="action"
-        >
-          <span>Submit</span>
-        </button>
+        <div>
+          <p className="reset-text">Enter your email address here.</p>
+          <form id="reset">
+            <input id="resetEmail" type="email" />
+            <button type="submit" className="btn reset-button">
+              <span>Submit</span>
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
