da9ebd4d03ef8912e9593ebc64bf17677520f291
Ebenezer <ebenezerdonu@gmail.com>
2019-02-14 00:16:03 +0100

    [Feature: #162727509] add test for ellipsisLoader

--- src/app/loaders/loaders.test.js
+++ src/app/loaders/loaders.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import RingLoaderComponent from './RingLoaderComponent';
+import EllipsisLoaderComponent from './EllipsisLoaderComponent';
 
 describe('It should render the RingLoader', () => {
-  it('should render the Landing Page', () => {
+  it('should render the RingLoaderComponent', () => {
     const component = shallow(<RingLoaderComponent />);
+    expect(component.exists()).toBe(true);
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe('It should render the EllipsisLoader', () => {
+  it('should render the EllipsisLoaderComponent', () => {
+    const component = shallow(<EllipsisLoaderComponent />);
     expect(component.exists()).toBe(true);
     expect(component).toMatchSnapshot();
   });
