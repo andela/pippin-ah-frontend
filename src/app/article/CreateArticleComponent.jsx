a4d84ae92946df062384acc93e8ca30cff5d537f
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-13 15:19:06 +0100

    [#163860915]: add article-create component

--- src/app/article/CreateArticleComponent.jsx
+++ src/app/article/CreateArticleComponent.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import Editor from './EditorComponent';
+import './create-article.scss';
 
 const CreateArticleComponent = () => {
-  return <div> CreateArticleComponent</div>;
+  return (
+    <div className="create-wrapper">
+      <div className="create-container">
+        <div className="content-container">
+          <div className="create-author-details">
+            <div className="author-photo" />
+            <div className="create-author-name">
+              <div className="create-auth-name">Tom Henkins</div>
+            </div>
+          </div>
+          <div className="create-form">
+            <form className="article-form">
+              <input type="text" placeholder="Title" />
+              <select name="category" className="browser-default" required>
+                <option value="" disabled selected>
+                  Select Category
+                </option>
+                <option value="mathematics">Mathematics</option>
+                <option value="arts">Arts</option>
+                <option value="science">Science</option>
+                <option value="engineering">Engineering</option>
+                <option value="technology">Technology</option>
+              </select>
+              <input type="text" placeholder="Description" />
+              <div className="create-article-text">
+                <Editor />
+              </div>
+              <div className="create-article-button">
+                <button type="submit">POST</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateArticleComponent;
---
+++ src/app/article/EditorComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import TinyMCE from 'react-tinymce';
+
+const Editor = () => {
+  return (
+    <TinyMCE
+      placeholder="<p>This is the initial content of the editor</p>"
+      config={{
+        plugins: 'autolink link image lists print preview',
+        toolbar: 'undo redo | bold italic | alignleft aligncenter alignright',
+        width: '100%',
+      }}
+    />
+  );
+};
+
+export default Editor;
