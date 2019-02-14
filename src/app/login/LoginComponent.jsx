6bc71ba2286ca3ee7bf92a828514cf747dea4106
xrole <xrolediamond@gmail.com>
2019-02-14 17:13:50 +0100

    [feature: #163845026] fix footer issue

--- src/app/login/LoginComponent.jsx
+++ src/app/login/LoginComponent.jsx
@@ -16,85 +16,87 @@ export const LoginComponent = ({ loginUser, loginState }) => {
   }
   return (
     <Fragment>
-      <div className="signIn-div-wrapper">
-        <div className="signIn-container">
-          <div className="close-button-div-signIn">
-            <p className="close-button-sign close"> X </p>
-          </div>
-          <div className="signIn-text">
-            Sign <span className="up">In</span>
-          </div>
-          <div className="signIn-wrapper">
-            <div className="social-media-icons-signIn">
-              <div className="btn-facebookk">
-                <div className="facebook-icon-div-signIn">
-                  <i className="fab fa-facebook-f" />
-                </div>
-                <div className="fb-text-wrapper-signIn">
-                  <span className="social-text-signIn">
-                    Sign in with Facebook
-                  </span>
-                </div>
-              </div>
-              <div className="btn-twitterr">
-                <div className="twitter-icon-div-signIn">
-                  <i className="fab fa-twitter" />
-                </div>
-                <div className="twitter-text-wrapper-signIn">
-                  <span className="social-text-signIn">
-                    Sign in with Twitter
-                  </span>
-                </div>
-              </div>
-              <div className="btn-googlee">
-                <div className="google-icon-div-signIn">
-                  <i className="fab fa-google" />
-                </div>
-                <div className="google-text-wrapper-signIn">
-                  <span className="social-text-signIn">
-                    Sign in with Google
-                  </span>
-                </div>
-              </div>
+      <div className="relative-div-signIn">
+        <div className="signIn-div-wrapper">
+          <div className="signIn-container">
+            <div className="close-button-div-signIn">
+              <p className="close-button-sign close"> X </p>
             </div>
-            <form className="login" onSubmit={onFormSubmit}>
-              <div className="input-groups-signIn">
-                <div className="form-icon-div-signIn">
-                  <i className="fas fa-envelope form-input-icons-signIn" />
+            <div className="signIn-text">
+              Sign <span className="up">In</span>
+            </div>
+            <div className="signIn-wrapper">
+              <div className="social-media-icons-signIn">
+                <div className="btn-facebookk">
+                  <div className="facebook-icon-div-signIn">
+                    <i className="fab fa-facebook-f" />
+                  </div>
+                  <div className="fb-text-wrapper-signIn">
+                    <span className="social-text-signIn">
+                      Sign in with Facebook
+                    </span>
+                  </div>
                 </div>
-                <input
-                  id="usernameOrEmail"
-                  type="text"
-                  name="usernameOrEmail"
-                  placeholder="Username or Email"
-                  required
-                />
-              </div>
-              <div className="input-groups-signIn">
-                <div className="form-icon-div-signIn">
-                  <i className="fas fa-key form-input-icons-signIn" />
+                <div className="btn-twitterr">
+                  <div className="twitter-icon-div-signIn">
+                    <i className="fab fa-twitter" />
+                  </div>
+                  <div className="twitter-text-wrapper-signIn">
+                    <span className="social-text-signIn">
+                      Sign in with Twitter
+                    </span>
+                  </div>
                 </div>
-                <input
-                  id="password"
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  required
-                />
+                <div className="btn-googlee">
+                  <div className="google-icon-div-signIn">
+                    <i className="fab fa-google" />
+                  </div>
+                  <div className="google-text-wrapper-signIn">
+                    <span className="social-text-signIn">
+                      Sign in with Google
+                    </span>
+                  </div>
+                </div>
               </div>
-              <button className="btn-submit-signIn" type="submit">
-                SIGN IN
-              </button>
-              <div className="sign-up-alternative-div-signIn">
-                <p className="sign-up-alternative-text-signIn">
-                  {' '}
-                  Forgort Password?
-                </p>
-              </div>
-            </form>
-          </div>
-          <div className="ring-loader">
-            {loginState === constants.LOGGING_IN && <RingLoaderComponent />}
+              <form className="login" onSubmit={onFormSubmit}>
+                <div className="input-groups-signIn">
+                  <div className="form-icon-div-signIn">
+                    <i className="fas fa-envelope form-input-icons-signIn" />
+                  </div>
+                  <input
+                    id="usernameOrEmail"
+                    type="text"
+                    name="usernameOrEmail"
+                    placeholder="Username or Email"
+                    required
+                  />
+                </div>
+                <div className="input-groups-signIn">
+                  <div className="form-icon-div-signIn">
+                    <i className="fas fa-key form-input-icons-signIn" />
+                  </div>
+                  <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    required
+                  />
+                </div>
+                <button className="btn-submit-signIn" type="submit">
+                  SIGN IN
+                </button>
+                <div className="sign-up-alternative-div-signIn">
+                  <p className="sign-up-alternative-text-signIn">
+                    {' '}
+                    Forgort Password?
+                  </p>
+                </div>
+              </form>
+            </div>
+            <div className="ring-loader">
+              {loginState === constants.LOGGING_IN && <RingLoaderComponent />}
+            </div>
           </div>
         </div>
       </div>
--- src/app/signup/SignupComponent.jsx
+++ src/app/signup/SignupComponent.jsx
@@ -24,118 +24,120 @@ const SignupComponent = ({ signupUser, signupState, errorMessage }) => {
 
   return (
     <Fragment>
-      <div className="signup-div-wrapper">
-        <div className="signup-divs">
-          <div className="close-button-div">
-            <p className="close-button close"> X </p>
-          </div>
-          <div className="signup-text">
-            Sign <span className="up">Up</span>
-          </div>
-          <div className="signup-wrapper">
-            <div className="social-media-icons">
-              <div className="btn-facebookk">
-                <div className="facebook-icon-div">
-                  <i className="fab fa-facebook-f" />
-                </div>
-                <div className="fb-text-wrapper">
-                  <span className="social-text">Sign up with Facebook</span>
-                </div>
-              </div>
-              <div className="btn-twitterr">
-                <div className="twitter-icon-div">
-                  <i className="fab fa-twitter" />
-                </div>
-                <div className="twitter-text-wrapper">
-                  <span className="social-text">Sign up with Twitter</span>
-                </div>
-              </div>
-              <div className="btn-googlee">
-                <div className="google-icon-div">
-                  <i className="fab fa-google" />
-                </div>
-                <div className="google-text-wrapper">
-                  <span className="social-text">Sign up with Google</span>
-                </div>
-              </div>
+      <div className="relative-div">
+        <div className="signup-div-wrapper">
+          <div className="signup-divs">
+            <div className="close-button-div">
+              <p className="close-button close"> X </p>
             </div>
-            <form onSubmit={onFormSubmit}>
-              <div className="input-groups">
-                <div className="form-icon-div">
-                  <i className="fas fa-envelope form-input-icons" />
+            <div className="signup-text">
+              Sign <span className="up">Up</span>
+            </div>
+            <div className="signup-wrapper">
+              <div className="social-media-icons">
+                <div className="btn-facebookk">
+                  <div className="facebook-icon-div">
+                    <i className="fab fa-facebook-f" />
+                  </div>
+                  <div className="fb-text-wrapper">
+                    <span className="social-text">Sign up with Facebook</span>
+                  </div>
                 </div>
-                <input
-                  className={
-                    !errorMessage.includes('Email') ? '' : 'input-error'
-                  }
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  required
-                />
-              </div>
-              <div className="input-groups">
-                <div className="form-icon-div">
-                  <i className="fas fa-user form-input-icons" />
+                <div className="btn-twitterr">
+                  <div className="twitter-icon-div">
+                    <i className="fab fa-twitter" />
+                  </div>
+                  <div className="twitter-text-wrapper">
+                    <span className="social-text">Sign up with Twitter</span>
+                  </div>
                 </div>
-                <input
-                  className={
-                    !errorMessage.includes('username') ? '' : 'input-error'
-                  }
-                  type="text"
-                  name="username"
-                  placeholder="Username"
-                  pattern=".{6,}"
-                  title="Username must be at least 6 characters"
-                  required
-                />
-              </div>
-              <div className="input-groups">
-                <div className="form-icon-div">
-                  <i className="fas fa-key form-input-icons" />
-                </div>
-                <input
-                  className={
-                    !errorMessage.includes('password') ? '' : 'input-error'
-                  }
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="Password"
-                  pattern=".{8,}"
-                  title="Password must be at least 8 characters"
-                  required
-                />
-              </div>
-              <div className="input-groups">
-                <div className="form-icon-div">
-                  <i className="fas fa-key form-input-icons" />
-                </div>
-                <input
-                  className={
-                    !errorMessage.includes('password') ? '' : 'input-error'
-                  }
-                  type="password"
-                  name="rePassword"
-                  placeholder="Confirm Password"
-                  required
-                />
-              </div>
-              <div>
-                <button className="btn-submit" type="submit">
-                  <span className="submit-text">SIGN UP</span>
-                </button>
-                <div className="sign-up-alternative-div">
-                  <p className="account">
-                    Have an account?
-                    <span className="sign-up-alternative-text">Sign In</span>
-                  </p>
+                <div className="btn-googlee">
+                  <div className="google-icon-div">
+                    <i className="fab fa-google" />
+                  </div>
+                  <div className="google-text-wrapper">
+                    <span className="social-text">Sign up with Google</span>
+                  </div>
                 </div>
               </div>
-            </form>
-          </div>
-          <div className="ring-loader">
-            {signupState === constants.SIGNING_UP && <RingLoaderComponent />}
+              <form onSubmit={onFormSubmit}>
+                <div className="input-groups">
+                  <div className="form-icon-div">
+                    <i className="fas fa-envelope form-input-icons" />
+                  </div>
+                  <input
+                    className={
+                      !errorMessage.includes('Email') ? '' : 'input-error'
+                    }
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </div>
+                <div className="input-groups">
+                  <div className="form-icon-div">
+                    <i className="fas fa-user form-input-icons" />
+                  </div>
+                  <input
+                    className={
+                      !errorMessage.includes('username') ? '' : 'input-error'
+                    }
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    pattern=".{6,}"
+                    title="Username must be at least 6 characters"
+                    required
+                  />
+                </div>
+                <div className="input-groups">
+                  <div className="form-icon-div">
+                    <i className="fas fa-key form-input-icons" />
+                  </div>
+                  <input
+                    className={
+                      !errorMessage.includes('password') ? '' : 'input-error'
+                    }
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    pattern=".{8,}"
+                    title="Password must be at least 8 characters"
+                    required
+                  />
+                </div>
+                <div className="input-groups">
+                  <div className="form-icon-div">
+                    <i className="fas fa-key form-input-icons" />
+                  </div>
+                  <input
+                    className={
+                      !errorMessage.includes('password') ? '' : 'input-error'
+                    }
+                    type="password"
+                    name="rePassword"
+                    placeholder="Confirm Password"
+                    required
+                  />
+                </div>
+                <div>
+                  <button className="btn-submit" type="submit">
+                    <span className="submit-text">SIGN UP</span>
+                  </button>
+                  <div className="sign-up-alternative-div">
+                    <p className="account">
+                      Have an account?
+                      <span className="sign-up-alternative-text">Sign In</span>
+                    </p>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="ring-loader">
+              {signupState === constants.SIGNING_UP && <RingLoaderComponent />}
+            </div>
           </div>
         </div>
       </div>
