d5c06ce4481deeb1ebd024ff467bff7505c62254
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-17 18:39:15 +0100

    [#162727514]: update webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -15,6 +15,14 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              {
+                plugins: ['@babel/plugin-proposal-class-properties'],
+              },
+            ],
+          },
         },
       },
       {
