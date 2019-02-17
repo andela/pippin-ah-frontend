c3adb79af017c1846980de33fba92491d9edfef6
Ebenezer <ebenezerdonu@gmail.com>
2019-02-17 23:46:52 +0100

    [Feature: #164012830] dispatch article data

--- src/app/subject/duck/operations.js
+++ src/app/subject/duck/operations.js
@@ -3,30 +3,28 @@ import { toast } from 'react-toastify';
 import actions from './actions';
 import constants from './constants';
 
-const { setFetchArticleState, setFetchArticleError } = actions;
+const { setFetchArticleState, setFetchArticleError, addArticleData } = actions;
 const category = window.location.pathname.split('/')[2];
 const url = 'https://learnground-api-staging.herokuapp.com/api/v1/articles';
 
 const doFetch = () => dispatch => {
   dispatch(setFetchArticleState(constants.FETCHING_ARTICLE));
   dispatch(setFetchArticleError(''));
-  return (
-    axios
-      .get(url, {
-        params: { category },
-      })
-      // eslint-disable-next-line no-unused-vars
-      .then(({ data }) => {
-        dispatch(setFetchArticleState(constants.FETCH_ARTICLE_SUCCESS));
-      })
-      .catch(({ response }) => {
-        dispatch(setFetchArticleState(constants.FETCH_ARTICLE_ERROR));
-        dispatch(setFetchArticleError(response.data.error));
-        toast.error(response.data.error, {
-          hideProgressBar: true,
-        });
-      })
-  );
+  return axios
+    .get(url, {
+      params: { category },
+    })
+    .then(({ data }) => {
+      dispatch(addArticleData(data));
+      dispatch(setFetchArticleState(constants.FETCH_ARTICLE_SUCCESS));
+    })
+    .catch(({ response }) => {
+      dispatch(setFetchArticleState(constants.FETCH_ARTICLE_ERROR));
+      dispatch(setFetchArticleError(response.data.error));
+      toast.error(response.data.error, {
+        hideProgressBar: true,
+      });
+    });
 };
 
 export default doFetch;
