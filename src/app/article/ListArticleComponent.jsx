bca22ee1e2ff9700264f42bb81f2528215cf3561
Ebenezer <ebenezerdonu@gmail.com>
2019-02-18 20:39:43 +0100

    [Feature: #164012830] populate div with articles

--- src/app/article/ListArticleComponent.jsx
+++ src/app/article/ListArticleComponent.jsx
@@ -11,12 +11,6 @@ const methods = {
 };
 
 const ListArticleComponent = ({ fetchArticleState, articleData }) => {
-  if (fetchArticleState === constants.FETCH_ARTICLE_SUCCESS) {
-    const articleList = articleData.articles;
-    articleList.forEach(article => {
-      console.log('****', article);
-    });
-  }
   return (
     <Fragment>
       <div className="container">
@@ -24,39 +18,39 @@ const ListArticleComponent = ({ fetchArticleState, articleData }) => {
           <h3>Arts</h3>
         </div>
         <div className="row">
-          <div className="col s12 m6 l4">
-            <div className="card">
-              <div className="card-image">
-                <img
-                  className="activator"
-                  alt="Cover"
-                  src="https://i.pinimg.com/564x/b2/69/9d/b2699de28deffa0883978ffecfd977a1.jpg?b=t"
-                />
-              </div>
-              <div className="card-content right-align">
-                <span className="card-title activator flow-text truncate center-align">
-                  How to Develop Your Ideas
-                </span>
-                <strong>by: Bugs Burney</strong>
-                <div>
-                  <a href="/#">Read more</a>
+          {fetchArticleState === constants.FETCH_ARTICLE_SUCCESS &&
+            articleData.articles.map(article => (
+              <div className="col s12 m6 l4" key={article.slug}>
+                <div className="card">
+                  <div className="card-image">
+                    <img
+                      className="activator"
+                      alt="Cover"
+                      src="https://i.pinimg.com/564x/b2/69/9d/b2699de28deffa0883978ffecfd977a1.jpg?b=t"
+                    />
+                  </div>
+                  <div className="card-content right-align">
+                    <span className="card-title activator flow-text truncate center-align">
+                      {article.title}
+                    </span>
+                    <strong>by: {article.author}</strong>
+                    <div>
+                      <a href="/#">Read more</a>
+                    </div>
+                  </div>
+                  <div className="card-reveal">
+                    <span className="card-title grey-text text-darken-4">
+                      {article.title}
+                      <i className="material-icons right">close</i>
+                    </span>
+                    <p>{article.description}</p>
+                    <div>
+                      <a href="/#">Click here to read</a>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">
-                  How to Develop Your Ideas
-                  <i className="material-icons right">close</i>
-                </span>
-                <p>
-                  This article explains how I learnt to share my ideas through
-                  art.
-                </p>
-                <div>
-                  <a href="/#">Click here to read</a>
-                </div>
-              </div>
-            </div>
-          </div>
+            ))}
         </div>
       </div>
     </Fragment>
