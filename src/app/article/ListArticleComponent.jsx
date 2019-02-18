3942d137f2815740c680eba0b594214b88974a0d
Ebenezer <ebenezerdonu@gmail.com>
2019-02-18 19:18:21 +0100

    [Feature: #164012830] get artilce data from api

--- src/app/article/ListArticleComponent.jsx
+++ src/app/article/ListArticleComponent.jsx
@@ -1,12 +1,22 @@
 /* eslint-disable max-len */
 import React, { Fragment } from 'react';
+import lifecycle from 'react-pure-lifecycle';
 import { constants } from './duck';
 import './ListArticle.scss';
 
-const SubjectComponent = ({ fetchArticle, fetchArticleState }) => {
-  fetchArticle();
-  console.log(fetchArticle());
-  console.log(fetchArticleState);
+const methods = {
+  componentDidMount({ fetchArticle }) {
+    fetchArticle();
+  },
+};
+
+const ListArticleComponent = ({ fetchArticleState, articleData }) => {
+  if (fetchArticleState === constants.FETCH_ARTICLE_SUCCESS) {
+    const articleList = articleData.articles;
+    articleList.forEach(article => {
+      console.log('****', article);
+    });
+  }
   return (
     <Fragment>
       <div className="container">
@@ -47,175 +57,10 @@ const SubjectComponent = ({ fetchArticle, fetchArticleState }) => {
               </div>
             </div>
           </div>
-          <div className="col s12 m6 l4">
-            <div className="card">
-              <div className="card-image">
-                <img
-                  className="activator"
-                  alt="Cover"
-                  src="https://i.pinimg.com/564x/b2/69/9d/b2699de28deffa0883978ffecfd977a1.jpg?b=t"
-                />
-              </div>
-              <div className="card-content right-align">
-                <span className="card-title activator flow-text truncate center-align">
-                  How to Develop Your Ideas
-                </span>
-                <strong>by: Bugs Burney</strong>
-                <div>
-                  <a href="/#">Read more</a>
-                </div>
-              </div>
-              <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">
-                  How to Develop Your Ideas
-                  <i className="material-icons right">close</i>
-                </span>
-                <p>
-                  This article explains how I learnt to share my ideas through
-                  art.
-                </p>
-                <div>
-                  <a href="/#">Click here to read</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col s12 m6 l4">
-            <div className="card">
-              <div className="card-image">
-                <img
-                  className="activator"
-                  alt="Cover"
-                  src="https://i.pinimg.com/564x/b2/69/9d/b2699de28deffa0883978ffecfd977a1.jpg?b=t"
-                />
-              </div>
-              <div className="card-content right-align">
-                <span className="card-title activator flow-text truncate center-align">
-                  How to Develop Your Ideas
-                </span>
-                <strong>by: Bugs Burney</strong>
-                <div>
-                  <a href="/#">Read more</a>
-                </div>
-              </div>
-              <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">
-                  How to Develop Your Ideas
-                  <i className="material-icons right">close</i>
-                </span>
-                <p>
-                  This article explains how I learnt to share my ideas through
-                  art.
-                </p>
-                <div>
-                  <a href="/#">Click here to read</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col s12 m6 l4">
-            <div className="card">
-              <div className="card-image">
-                <img
-                  className="activator"
-                  alt="Cover"
-                  src="https://i.pinimg.com/564x/b2/69/9d/b2699de28deffa0883978ffecfd977a1.jpg?b=t"
-                />
-              </div>
-              <div className="card-content right-align">
-                <span className="card-title activator flow-text truncate center-align">
-                  How to Develop Your Ideas
-                </span>
-                <strong>by: Bugs Burney</strong>
-                <div>
-                  <a href="/#">Read more</a>
-                </div>
-              </div>
-              <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">
-                  How to Develop Your Ideas
-                  <i className="material-icons right">close</i>
-                </span>
-                <p>
-                  This article explains how I learnt to share my ideas through
-                  art.
-                </p>
-                <div>
-                  <a href="/#">Click here to read</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col s12 m6 l4">
-            <div className="card">
-              <div className="card-image">
-                <img
-                  className="activator"
-                  alt="Cover"
-                  src="https://i.pinimg.com/564x/b2/69/9d/b2699de28deffa0883978ffecfd977a1.jpg?b=t"
-                />
-              </div>
-              <div className="card-content right-align">
-                <span className="card-title activator flow-text truncate center-align">
-                  How to Develop Your Ideas
-                </span>
-                <strong>by: Bugs Burney</strong>
-                <div>
-                  <a href="/#">Read more</a>
-                </div>
-              </div>
-              <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">
-                  How to Develop Your Ideas
-                  <i className="material-icons right">close</i>
-                </span>
-                <p>
-                  This article explains how I learnt to share my ideas through
-                  art.
-                </p>
-                <div>
-                  <a href="/#">Click here to read</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col s12 m6 l4">
-            <div className="card">
-              <div className="card-image">
-                <img
-                  className="activator"
-                  alt="Cover"
-                  src="https://i.pinimg.com/564x/b2/69/9d/b2699de28deffa0883978ffecfd977a1.jpg?b=t"
-                />
-              </div>
-              <div className="card-content right-align">
-                <span className="card-title activator flow-text truncate center-align">
-                  How to Develop Your Ideas
-                </span>
-                <strong>by: Bugs Burney</strong>
-                <div>
-                  <a href="/#">Read more</a>
-                </div>
-              </div>
-              <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">
-                  How to Develop Your Ideas
-                  <i className="material-icons right">close</i>
-                </span>
-                <p>
-                  This article explains how I learnt to share my ideas through
-                  art.
-                </p>
-                <div>
-                  <a href="/#">Click here to read</a>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </Fragment>
   );
 };
 
-export default SubjectComponent;
+export default lifecycle(methods)(ListArticleComponent);
