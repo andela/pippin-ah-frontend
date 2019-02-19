580cec0544c1e0495439193dd41797ef7694dbb8
Ebenezer <ebenezerdonu@gmail.com>
2019-02-19 15:20:03 +0100

    [Feature: #164012830] add createStore for redux dev tool

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'materialize-css';
 import 'materialize-css/dist/css/materialize.min.css';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,7 +11,11 @@ import '../node_modules/materialize-css/dist/js/materialize.min';
 import App from './app/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk)),
+);
 
 ReactDOM.render(
   <Provider store={store}>
