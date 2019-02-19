31cae0626c9dae23fcad52c977ddd3be0edfd4b1
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-19 16:22:03 +0100

    [#164047234]: update for image upload

--- src/app/article/duck/operations.js
+++ src/app/article/duck/operations.js
@@ -1,29 +1,51 @@
 import axios from 'axios';
 import actions from './actions';
 import constants from './constants';
+import { uploadImage } from '../../util/uploadToCloudinary';
 
 const url = 'https://learnground-api-staging.herokuapp.com/api/v1/articles';
 const { setCreateStatus } = actions;
 const doCreateArticle = articleDetails => dispatch => {
+  if (!articleDetails.uploadCoverUrl) {
+    return dispatch(
+      setCreateStatus({
+        status: constants.CREATE_ERROR,
+        data: 'You must select a cover image',
+      }),
+    );
+  }
   dispatch(setCreateStatus({ status: constants.CREATING }));
-  const headers = {
-    headers: { Authorization: localStorage.getItem('token') },
-  };
-  return axios
-    .post(url, articleDetails, headers)
-    .then(({ data }) => {
-      dispatch(
-        setCreateStatus({
-          status: constants.CREATE_SUCCESS,
-          data: data.slug,
-        }),
-      );
+  const { uploadCoverUrl } = articleDetails;
+  uploadImage('article', uploadCoverUrl)
+    .then(imageLink => {
+      articleDetails.coverImageUrl = imageLink;
+      const headers = {
+        headers: { Authorization: localStorage.getItem('token') },
+      };
+      return axios
+        .post(url, articleDetails, headers)
+        .then(({ data }) => {
+          return dispatch(
+            setCreateStatus({
+              status: constants.CREATE_SUCCESS,
+              data: data.slug,
+            }),
+          );
+        })
+        .catch(({ response }) => {
+          return dispatch(
+            setCreateStatus({
+              status: constants.CREATE_ERROR,
+              data: response.data.error,
+            }),
+          );
+        });
     })
-    .catch(({ response }) => {
-      dispatch(
+    .catch(() => {
+      return dispatch(
         setCreateStatus({
           status: constants.CREATE_ERROR,
-          data: response.data.error,
+          data: 'error uploading cover image',
         }),
       );
     });
