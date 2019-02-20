0ec31612cefd1cbd14a45facedc1968a3f93e6e5
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-20 15:24:11 +0100

    [#164101954]: refactor exports

--- src/app/article/duck/index.js
+++ src/app/article/duck/index.js
@@ -1,4 +1,4 @@
-export { default as doCreateArticle } from './operations';
+export { doCreateArticle, doFetchArticle } from './operations';
 export { default as createArticleReducer } from './reducers';
 export { default as constants } from './constants';
 export { default as types } from './types';
