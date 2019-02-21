fe312fe6e981607cf44332ab7e6a9a80d306132d
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-21 13:48:11 +0100

    [#164101954]: update exports

--- src/app/article/duck/index.js
+++ src/app/article/duck/index.js
@@ -3,3 +3,4 @@ export { default as createArticleReducer } from './reducers';
 export { default as constants } from './constants';
 export { default as types } from './types';
 export { default as actions } from './actions';
+export { default as formatDate } from './formateDate';
