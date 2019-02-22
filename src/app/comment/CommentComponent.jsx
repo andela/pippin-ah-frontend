785e3ea01f730d554daee8988cb4c3c960911954
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-22 10:03:00 +0100

    [#164101954]: setup comments

---
+++ src/app/comment/CommentComponent.jsx
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react';
+
+const CommentComponent = ({ commentDetails }) => {
+  return (
+    <Fragment>
+      <div className="comment-box">
+        <div className="header-wrapper">
+          <div className="comment-owner">
+            <Fragment>
+              {commentDetails.author.imageUrl ? (
+                <img
+                  src={commentDetails.author.imageUrl}
+                  className="owner-image"
+                  alt="comment-author"
+                />
+              ) : (
+                <i className="fas fa-user-circle" />
+              )}
+            </Fragment>
+          </div>
+          <div className="comment-container">
+            <div className="comment-header">
+              <span className="comment-author">
+                {commentDetails.author.lastName
+                  ? `${commentDetails.author.firstName} ${
+                      commentDetails.author.lastName
+                    }`
+                  : commentDetails.author.username}
+              </span>
+              <span className="comment-date">
+                {Object.keys(commentDetails.comment)[0]}
+              </span>
+            </div>
+            <p className="comment-text">
+              {commentDetails.comment[Object.keys(commentDetails.comment)[0]]}
+            </p>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default CommentComponent;
