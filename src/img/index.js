829f9f7e31945916c71c792dd8e465037df92dbc
habibaudu <auduhabib1990@gmail.com>
2019-02-28 17:30:08 +0100

    feat(view-and-edit-profile): fix heroku build error [#162727513]

--- src/img/index.js
+++ src/img/index.js
@@ -1,6 +1,6 @@
 export { default as cancel } from './cancel.svg';
-export { default as facebook } from './Facebook.svg';
-export { default as twitter } from './Twitter.svg';
+export { default as facebook } from './facebook.svg';
+export { default as twitter } from './twitter.svg';
 export { default as googleplus } from './Googleplus.svg';
 export { default as email } from './email.svg';
 export { default as lock } from './lock.svg';
