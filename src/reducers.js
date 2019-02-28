55ce97837023a53cd78a2846045e198e982caed5
Ebenezer <ebenezerdonu@gmail.com>
2019-02-28 13:51:02 +0100

    [feature: #162727517] add bookmark article reducer

--- src/reducers.js
+++ src/reducers.js
@@ -3,7 +3,11 @@ import { signupReducer } from './app/signup';
 import { loginReducer } from './app/login';
 import { resetPasswordReducer } from './app/resetPassword';
 import { newPasswordReducer } from './app/newPassword';
-import { createArticleReducer, fetchArticleReducer } from './app/article';
+import {
+  createArticleReducer,
+  fetchArticleReducer,
+  bookmarkArticleReducer,
+} from './app/article';
 
 const rootReducer = combineReducers({
   signup: signupReducer,
@@ -12,6 +16,7 @@ const rootReducer = combineReducers({
   updatePassword: newPasswordReducer,
   createArticle: createArticleReducer,
   fetchArticle: fetchArticleReducer,
+  bookmarkArticle: bookmarkArticleReducer,
 });
 
 export default rootReducer;
