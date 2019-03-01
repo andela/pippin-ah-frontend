9be10ec595984266c107b6539f6f99f9d2d9ab9e
Emmanuel Raji <rojo.mosco@gmail.com>
2019-03-01 10:59:45 +0100

    [#162727532]: add comment highlights

--- src/app/article/ArticleContainer.js
+++ src/app/article/ArticleContainer.js
@@ -1,13 +1,17 @@
 import { connect } from 'react-redux';
 import ArticleComponent from './ArticleComponent';
-import { doFetchArticle } from './duck';
+import { doFetchArticle, doUploadHighlight } from './duck';
 
-const mapStateToProps = ({ createArticle: { singleFetchStatus } }) => {
-  return { singleFetchStatus };
+const mapStateToProps = ({
+  createArticle: { singleFetchStatus, highlightUploadStatus },
+}) => {
+  return { singleFetchStatus, highlightUploadStatus };
 };
 const mapDispatchToProps = dispatch => {
   return {
     fetchSingleArticle: slug => dispatch(doFetchArticle(slug)),
+    uploadHighlightedText: uploadObject =>
+      dispatch(doUploadHighlight(uploadObject)),
   };
 };
 
